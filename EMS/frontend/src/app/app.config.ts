f19224d5ed5b35bd1c89f0257153eb4d9ee10c87
mahir-hs <mahirhasan333@gmail.com>
2025-01-07 14:42:21 +0600

    update: changed the way of interceptor. properly configured in the app config, instead of main ts file

--- EMS/frontend/src/app/app.config.ts
+++ EMS/frontend/src/app/app.config.ts
@@ -3,13 +3,18 @@ import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideClientHydration } from '@angular/platform-browser';
-import { provideHttpClient, withFetch } from '@angular/common/http';
+import {
+  provideHttpClient,
+  withFetch,
+  withInterceptors,
+} from '@angular/common/http';
+import { tokenInterceptor } from './interceptor/token.interceptor';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }),
-     provideRouter(routes),
-     provideClientHydration(),
-     provideHttpClient(withFetch())
-    ]
+    provideRouter(routes),
+    provideClientHydration(),
+    provideHttpClient(withFetch(), withInterceptors([tokenInterceptor])),
+  ],
 };
--- EMS/frontend/src/main.ts
+++ EMS/frontend/src/main.ts
@@ -1,15 +1,7 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
-import { provideHttpClient, withInterceptors } from '@angular/common/http';
-import { tokenInterceptor } from './app/interceptor/token.interceptor';
-import { provideRouter } from '@angular/router';
-import { routes } from './app/app.routes';
 
-const config = {
-  providers: [
-    provideHttpClient(withInterceptors([tokenInterceptor])),
-    provideRouter(routes),
-  ],
-};
-bootstrapApplication(AppComponent, config).catch((err) => console.error(err));
+bootstrapApplication(AppComponent, appConfig).catch((err) =>
+  console.error(err)
+);
